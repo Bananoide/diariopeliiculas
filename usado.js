7b4cc3d919e56f6fc39ae15b59ed01a749ed03cf
Bananoide <83323196+Bananoide@users.noreply.github.com>
2024-05-13 19:05:23 -0600

    Update usado.js

--- usado.js
+++ usado.js
@@ -8,24 +8,25 @@ function myFunction(){
         }
     }
 
-var audio=document.getElementById("miAudio");
-var vol=document.getElementById("miVol");
+ var audio = document.getElementById("miAudio");
+  var vol = document.getElementById("miVol");
+  var subirBoton = document.getElementById("subirVol");
+  var bajarBoton = document.getElementById("bajarVol");
 
-vol.addEventListener("input", function(){
-	audio.volume= vol.value;
-});
-
-var sub = document.getElementById("subirVol");
-  var baj = document.getElementById("bajarVol");
+  vol.addEventListener("input", function() {
+    audio.volume = vol.value;
+  });
 
   subirBoton.addEventListener("click", function() {
     if (audio.volume < 1) {
       audio.volume += 0.1;
+      vol.value = audio.volume;
     }
   });
 
   bajarBoton.addEventListener("click", function() {
     if (audio.volume > 0) {
       audio.volume -= 0.1;
+      vol.value = audio.volume;
     }
   });
