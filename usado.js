8e9e5bf6652d95e48cada54b6e5b37fdeb2b80b4
Bananoide <83323196+Bananoide@users.noreply.github.com>
2024-05-13 18:13:30 -0600

    Update usado.js

--- usado.js
+++ usado.js
@@ -14,3 +14,18 @@ var vol=document.getElementById("miVol");
 vol.addEventListener("input", function(){
 	audio.volume= vol.value;
 });
+
+var sub = document.getElementById("subirVol");
+  var baj = document.getElementById("bajarVol");
+
+  subirBoton.addEventListener("click", function() {
+    if (audio.volume < 1) {
+      audio.volume += 0.1;
+    }
+  });
+
+  bajarBoton.addEventListener("click", function() {
+    if (audio.volume > 0) {
+      audio.volume -= 0.1;
+    }
+  });
